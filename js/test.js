681f87147c6d556e44c32cec835e55eecc8b0e55
VitalikGrischenko <110487381+VitalikGrischenko@users.noreply.github.com>
2023-09-10 10:49:12 +0300

    sehszsagy

--- js/test.js
+++ js/test.js
@@ -1,22 +1,22 @@
-// const user = {
-//     name:"Anton",
-//     pfor:"teacher",
-//     showname() {console.log('Hello I am your new teacher my name is ' + this.name)}
+const user = {
+    name:"Anton",
+    pfor:"teacher",
+    showname() {console.log('Hello my name is ' + this.name)}
 
-// };
+};
 
-// user.showname();
+user.showname();
 
-// const user1 = {
-//     name:"Atnon",
-// };
+const user1 = {
+    name:"Artem",
+};
 
-// user.showname.call(user1);
-// //user.showname.apply(user1) це одне й те сам але зроблено через call і apply
+user.showname.call(user1);
+//user.showname.apply(user1) це одне й те сам але зроблено через call і apply
 
-// user1.showname = user.showname.bind(user1);
+user1.showname = user.showname.bind(user1);
 
-// user1.showname();
+user1.showname();
 
 const numbers = [1, 2, 5]
 //console.log(Math.max(numbers));  буде NaN
